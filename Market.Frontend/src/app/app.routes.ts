7f0d79c6b4ec2141d3dd3715571cbb35110e7b3a
Adna Dušić <adna.dusic@edu.fit.ba>
2026-08-24 22:05:53 +0200

    feat: add custom landing page

--- Market.Frontend/src/app/app.routes.ts
+++ Market.Frontend/src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 
 import { authGuard } from './core/guards/auth.guard';
 import { Login } from './features/auth/login/login';
-import { BookList } from './features/books/book-list/book-list';
-import { BookForm } from './features/books/book-form/book-form';
 import { BookDetailsPage } from './features/books/book-details/book-details';
+import { BookForm } from './features/books/book-form/book-form';
+import { BookList } from './features/books/book-list/book-list';
+import { Landing } from './features/landing/landing';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'books',
-    pathMatch: 'full',
+    component: Landing,
   },
   {
     path: 'login',
@@ -36,6 +36,6 @@ export const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: 'books',
+    redirectTo: '',
   },
 ];
