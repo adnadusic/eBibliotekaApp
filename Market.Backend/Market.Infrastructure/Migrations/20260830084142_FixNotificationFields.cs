662234b3c90ed819c06d10e02b305240e25eff95
Adna Dušić <adna.dusic@edu.fit.ba>
2026-08-30 10:48:03 +0200

    Ispravi polja obavijesti i EF migraciju

---
+++ Market.Backend/Market.Infrastructure/Migrations/20260830084142_FixNotificationFields.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Market.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNotificationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VezanoZaId",
+                table: "Obavijesti");
+
+            migrationBuilder.DropColumn(
+                name: "VezanoZaTip",
+                table: "Obavijesti");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Procitano",
+                table: "Obavijesti",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Procitano",
+                table: "Obavijesti",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AddColumn<int>(
+                name: "VezanoZaId",
+                table: "Obavijesti",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VezanoZaTip",
+                table: "Obavijesti",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
